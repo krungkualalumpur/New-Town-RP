d8e8441b12e6cf2f220fb371879ea70219ef4796
krungkualalumpur <86681413+krungkualalumpur@users.noreply.github.com>
2023-07-07 19:36:32 +0700

    Interactable System Setup

--- Scripts/CommandBarScripts/WeldWindows.lua
+++ Scripts/CommandBarScripts/WeldWindows.lua
@@ -1,6 +1,7 @@
 --!strict
 local CollectionService = game:GetService("CollectionService")
 
+ -- for windows
 for _,v in pairs(CollectionService:GetTagged("Window")) do
     local windowModel = v:FindFirstChild("Model")
     if windowModel then
@@ -13,6 +14,38 @@ for _,v in pairs(CollectionService:GetTagged("Window")) do
             end
 
             for _,v in pairs(windowModel:GetChildren()) do
+                if v ~= mainPart and v:IsA("BasePart") then
+                    local weld = Instance.new("WeldConstraint")
+                    weld.Part0 = mainPart
+                    weld.Part1 = v
+                    weld.Parent = mainPart
+
+                    for _,weld2 in pairs(v:GetChildren()) do
+                        if weld2:IsA("WeldConstraint") then
+                            weld2:Destroy()
+                        end
+                    end
+
+                    v.Anchored = false
+                end
+            end
+        end
+    end
+end
+
+ -- for doors
+ for _,v in pairs(CollectionService:GetTagged("Door")) do
+    local doorModel = v:FindFirstChild("Model")
+    if doorModel then
+        local mainPart = doorModel:GetChildren()[1]
+        if mainPart then
+            for _,v in pairs(mainPart:GetChildren()) do
+                if v:IsA("WeldConstraint") then
+                    v:Destroy()
+                end
+            end
+
+            for _,v in pairs(doorModel:GetChildren()) do
                 if v ~= mainPart and v:IsA("BasePart") then
                     local weld = Instance.new("WeldConstraint")
                     weld.Part0 = mainPart
---
+++ src/Shared/Interactable/init.lua
@@ -0,0 +1,14 @@
+--!strict
+--services
+--packages
+--modules
+--types
+--constants
+--references
+--variables
+--class
+local Interactable = {}
+function Interactable.new()
+    
+end
+return Interactable
